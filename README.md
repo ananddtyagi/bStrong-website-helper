b83f5c2e9b0aad82a34ae32c78578df1e033c892
Anand Tyagi <anand.deep.tyagi@gmail.com>
2022-11-09 23:02:36 -0800

    get url for each sig up button

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Notes:
+
+If you get an access warning in your console, make sure to disable any ad-blockers and go to inspect -> settings (gear icon) -> disable "Enable JavaScript source maps"
+https://stackoverflow.com/questions/61205390/how-can-i-fix-the-devtools-failed-to-load-sourcemap-could-not-load-content-er
+
--- background.js
+++ background.js
@@ -6,17 +6,38 @@ const filter = {
   ],
 };
 
-
-
 function displayAvailableTimes() {
+    let url_base = "https://clients.mindbodyonline.com"
+
     function checkSignIn() {
       let signInButton = document.getElementById("btnSignIn");
       if (signInButton !== null) {
           alert("Please sign in to use this extension");
+            return false
       }
+      return true
     }
-  checkSignIn();
- 
+
+    function addCheckIfAvailable() {
+        let els = document.getElementsByClassName("SignupButton");
+        Array.from(els).forEach((el) => {
+            let start = el.outerHTML.indexOf("/ASP")
+            let end = el.outerHTML.indexOf("';\"")
+            url_path = url_base + el.outerHTML.substring(start, end)
+            url_path = url_path.replaceAll("amp", "").replaceAll(";", "")
+            console.log(url_path)
+        });
+
+    }
+
+
+
+    if (!checkSignIn()){
+        return
+    }
+    console.log("You're signed in already, great!")
+    addCheckIfAvailable()
+
 }
 
 chrome.action.onClicked.addListener((tab) => {
